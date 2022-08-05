5815021c36eb980eb4608c22662258539842a001
Andres Catano <acatano@ac4digital.com>
2022-08-04 20:34:05 -0500

    first commit

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Method that determines if all the boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    keys = []
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes):
+                keys.append(box)
+    if len(keys) == len(boxes):
+        return True
+    else:
+        return False
