1d7f708a50248f743a28109658cb63690fb847e2
Andres Catano <acatano@ac4digital.com>
2022-11-24 18:11:11 -0500

    First Commit

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
+
+// Write a script that prints all characters of a Star Wars movie:
+// The first positional argument passed is the Movie ID - example: 3 = “Return of the Jedi”
+// Display one character name per line in the same order as the “characters” list in the /films/ endpoint
+// You must use the Star wars API
+// You must use the request module
+
 const request = require('request');
 
 const movieId = process.argv[2];
