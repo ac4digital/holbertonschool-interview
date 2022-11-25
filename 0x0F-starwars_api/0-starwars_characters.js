3cd1c51f8bd3d300ea3e90d2f8f0ae8398408808
Andres Catano <acatano@ac4digital.com>
2022-11-24 19:44:54 -0500

    First Commit

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,31 +1,17 @@
 #!/usr/bin/node
-
-// Write a script that prints all characters of a Star Wars movie:
-// The first positional argument passed is the Movie ID - example: 3 = “Return of the Jedi”
-// Display one character name per line in the same order as the “characters” list in the /films/ endpoint
-// You must use the Star wars API
-// You must use the request module
-
 const request = require('request');
-
-const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
-request(url, async function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
+  if (!err) {
     const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      const res = await new Promise((resolve, reject) => {
-        request(character, (error, res, html) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(JSON.parse(html).name);
-          }
-        });
-      });
-      console.log(res);
-    }
+    recursionRequestPrint(characters, 0);
   }
 });
+
+function recursionRequestPrint (url, index) {
+  request(url[index], (err, res, body) => {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < url.length) recursionRequestPrint(url, ++index);
+    }
+  });
+}
